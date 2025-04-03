7ef3627a82a0577ba4c05f60a12f984ccaf2e01c
wayne-bai-ics4u1-02-25 <wayne.bai@student.tdsb.on.ca>
2025-04-03 15:46:45 +0000

    qwertyuiopasdfghjklzxcvbnm

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,4 @@
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -24,4 +25,23 @@ public class PracticeProblem {
 		//Write question 5 code here
 	}
 
+	public static int towerOfHanoi(int towerSize) {
+		if (towerSize < 3) {
+			return -1;
+		}
+		return (int) Math.pow(2, towerSize) - 1;
+		// I would've been more interested if it asked us to actually perform the task.
+	}
 }
+
+/*
+[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=18998609)
+# Instructions  
+
+Create a function called towerOfHanoi(int num). Use recursion to return the number of moves it will take complete the towerOfHanoi problem with num amount of rings.
+
+return -1 if they input a number less than 3.
+
+Spend your time figuring out the problem and ensure you are using the absolute minimum number of moves. Use recursion!
+
+*/
